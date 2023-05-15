c43ec5d0e2cc887da214dd924e607fa307874340
Eric Maya <ericmaya8a@gmail.com>
2023-05-15 13:25:11 -0600

    chore: Update Students component

--- app/admin/students/ActionButton.tsx
+++ app/admin/students/ActionButton.tsx
@@ -1,43 +1,19 @@
-import { Address, Communication, Student } from "@prisma/client";
-import { useRouter } from "next/navigation";
-import { useRef, useState } from "react";
 import { PrimeIcons } from "primereact/api";
-import { Dialog } from "primereact/dialog";
 import { SpeedDial } from "primereact/speeddial";
-import { Toast } from "primereact/toast";
 import { Tooltip } from "primereact/tooltip";
 import styled from "styled-components";
-import { StudentForm } from "./StudentForm";
 
 type ActionButtonProps = {
-  createStudent: (
-    student: Omit<Student, "id" | "active"> &
-      Omit<Address, "id" | "studentId"> &
-      Omit<Communication, "id" | "studentId">
-  ) => Promise<{ ok: boolean }>;
+  handleOpenDialog: VoidFunction;
+  refreshPage: VoidFunction;
 };
 
-export function ActionButton({ createStudent }: ActionButtonProps) {
-  const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
-  const toast = useRef<Toast>(null);
-
-  const handleClose = () => setIsOpen(false);
-
-  const refreshPage = () => router.refresh();
-
-  const showToast = (message: string) => {
-    toast.current?.show({
-      severity: "success",
-      summary: "Success",
-      detail: message,
-    });
-    refreshPage();
-  };
-
+export function ActionButton({
+  handleOpenDialog,
+  refreshPage,
+}: ActionButtonProps) {
   return (
     <ActionButtonWrapper>
-      <Toast position="top-center" ref={toast} />
       <Tooltip target=".p-speeddial-action" position="left" />
       <SpeedDial
         radius={120}
@@ -50,7 +26,7 @@ export function ActionButton({ createStudent }: ActionButtonProps) {
           {
             label: "Add",
             icon: PrimeIcons.PLUS,
-            command: () => setIsOpen(true),
+            command: handleOpenDialog,
           },
           {
             label: "Refresh",
@@ -59,19 +35,6 @@ export function ActionButton({ createStudent }: ActionButtonProps) {
           },
         ]}
       />
-      <Dialog
-        header="Add Student"
-        visible={isOpen}
-        onHide={handleClose}
-        breakpoints={{ "960px": "75vw", "641px": "100vw" }}
-        style={{ minWidth: "50vw" }}
-      >
-        <StudentForm
-          createStudent={createStudent}
-          handleClose={handleClose}
-          handleToast={showToast}
-        />
-      </Dialog>
     </ActionButtonWrapper>
   );
 }
--- app/admin/students/Students.tsx
+++ app/admin/students/Students.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { Address, Communication, Student } from "@prisma/client";
+import { useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Dialog } from "primereact/dialog";
+import { Toast } from "primereact/toast";
 import { PageHeader } from "@/app/components/commons/PageHeader";
 import { IncativeStudentsTable } from "./IncativeStudentsTable";
 import { ActiveStudentsTable } from "./ActiveStudentsTable";
 import { ActionButton } from "./ActionButton";
+import { StudentForm } from "./StudentForm";
 
 type StudentsProps<T> = {
   students: T;
@@ -16,14 +21,51 @@ type StudentsProps<T> = {
 };
 
 export function Students<T>(props: StudentsProps<T>) {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const toast = useRef<Toast>(null);
+
+  const handleClose = () => setIsOpen(false);
+
+  const refreshPage = () => router.refresh();
+
+  const showToast = (message: string) => {
+    toast.current?.show({
+      severity: "success",
+      summary: "Success",
+      detail: message,
+    });
+    refreshPage();
+  };
+
   return (
     <>
       <PageHeader
         title="Students"
-        actions={<ActionButton createStudent={props.createStudent} />}
+        actions={
+          <ActionButton
+            handleOpenDialog={() => setIsOpen(true)}
+            refreshPage={refreshPage}
+          />
+        }
       />
       <ActiveStudentsTable students={props.students} />
       <IncativeStudentsTable students={props.students} />
+
+      <Toast position="top-center" ref={toast} />
+      <Dialog
+        header="Add Student"
+        visible={isOpen}
+        onHide={handleClose}
+        breakpoints={{ "960px": "75vw", "641px": "100vw" }}
+        style={{ minWidth: "50vw" }}
+      >
+        <StudentForm
+          createStudent={props.createStudent}
+          handleClose={handleClose}
+          handleToast={showToast}
+        />
+      </Dialog>
     </>
   );
 }
