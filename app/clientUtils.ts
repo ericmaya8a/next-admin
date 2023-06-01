6f2fce992611714de167a8272d74979ec191fcd2
Eric Maya <ericmaya8a@gmail.com>
2023-06-01 12:41:58 -0600

    chore: remove unused code

--- app/clientUtils.ts
+++ app/clientUtils.ts
@@ -1,37 +1,6 @@
-import { Rank, Student } from "@prisma/client";
+import { Rank } from "@prisma/client";
 import { SelectItemOptionsType } from "primereact/selectitem";
 import { StudentT } from "./admin/students/page";
-
-export const clientUtils = {
-  fetching: {
-    async post<T>(url: string, body: T) {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ ...body }),
-      });
-
-      return await response.json();
-    },
-  },
-};
-
-export function mapStudents(students: Student[]) {
-  return students.map(
-    ({ firstName, lastName, birthDate, inscriptionDate, ...other }) => ({
-      name: `${firstName} ${lastName}`,
-      birthDate: birthDate.toLocaleDateString("es-MX", { dateStyle: "medium" }),
-      inscriptionDate: inscriptionDate.toLocaleDateString("es-MX", {
-        dateStyle: "medium",
-      }),
-      ...other,
-    })
-  );
-}
-
-export type MapStudentsT = ReturnType<typeof mapStudents>;
 
 export function capitalizeEnum(enumValue: string) {
   const words = enumValue
